3ff7257e62b838576ded115622362478f6997d5b
AndreasKrammer <a-krammer@online.de>
2023-03-19 22:06:09 +0100

    Tried to implement H2 Database and DAO-Controller setup for an employee - Check bug

---
+++ src/main/java/com/entity/Employee.java
@@ -0,0 +1,16 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class Employee {
+    @Id
+    private int id;
+}
